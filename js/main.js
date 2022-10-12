784a2460aa1b876791336ee46b3cd6908c6a03bd
Лида Рудова <102348531+Venyak@users.noreply.github.com>
2022-10-12 16:28:47 +0500

    added animation to modal

--- js/main.js
+++ js/main.js
@@ -3,11 +3,33 @@ const pageOverlayModal = document.querySelector('.page__overlay_modal');
 const modalClose = document.querySelector('.modal__close');
 const pageOverlay = document.querySelector('.page__overlay');
 
-const handlerModal = (trigger, modal, openSelector) => {
-  trigger.addEventListener('click', () => {
-    modal.classList.toggle(openSelector);
+const handlerModal = (openBtn, modal, openSelector, closeBtn, speed) => {
+  let opacity = 0;
+
+  openBtn.addEventListener('click', () => {
+    modal.style.opacity = opacity;
+
+    modal.classList.add(openSelector);
+
+    const timer = setInterval(() => {
+      opacity += 0.05;
+      modal.style.opacity = opacity;
+      if (opacity >= 1) clearInterval(timer);
+    }, speed);
+  });
+
+  closeBtn.addEventListener('click', () => {
+    modal.style.opacity = opacity;
+
+    const timer = setInterval(() => {
+      opacity -= 0.05;
+      modal.style.opacity = opacity;
+      if (opacity <= 0) {
+        clearInterval(timer);
+        modal.classList.remove(openSelector);
+      }
+    }, speed);
   });
 };
 
-handlerModal(presentOrderBtn, pageOverlayModal, 'page__overlay_modal_open');
-handlerModal(modalClose, pageOverlayModal, 'page__overlay_modal_open');
+handlerModal(presentOrderBtn, pageOverlayModal, 'page__overlay_modal_open', modalClose, 6);
